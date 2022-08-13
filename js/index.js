d3f5c581e54693ad700d9a6f2c66676971ee9197
Rama Salazar <ramiroesalazar@gmail.com>
2022-08-13 19:45:56 -0300

    prueba btn ver mas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Class
 class Products{
     constructor(id, product, description, price, img){
         this.id = id,
@@ -12,21 +13,35 @@ class Products{
     }
 }
 
-let article = []
-const product1 = new Products(1,"Poncho","poncho adulto de hilo de 1.8 mts", 6000, "./img/poncho.png")
-article.push(product1)
-const product2 = new Products(2,"Ruana","ruana adulto de hilo de 1.8 mts", 5500,"./img/ruana.png")
-article.push(product2)
-const product3 = new Products(3,"Alpargatas","alpargatas de eco-cuero o gabardina. Talle 35 - 43", 2000,"./img/alpargata1.png")
-article.push(product3)
-const product4 = new Products(4,"Remera","remera de algodon primera calidad. Talle S - XXL", 1400,"./img/remera2.png")
-article.push(product4)
-const product5 = new Products(5,"Bombacha de gaucho","bombacha de gaucho de gabarnida primera calidad con bordaos", 6500, "./img/bombacha1.png")
-article.push(product5)
-const product6 = new Products(6,"Boina","boinas de hilo de adulto o nio marca -El llano-", 2300, "./img/boina.png")
-article.push(product6)
+// Arrays
+// Ropa
 
-const stock = [product1,product2,product3,product4,product5,product6]
+let clothing = []
+const cloth2 = new Products(4,"Remera","remera de algodon primera calidad. Talle S - XXL", 1400,"./img/remera2.png")
+clothing.push(cloth2)
+const cloth3 = new Products(5,"Bombacha de gaucho","bombacha de gaucho de gabarnida primera calidad con bordaos", 6500, "./img/bombacha1.png")
+clothing.push(cloth3)
+const cloth1 = new Products(3,"Alpargatas","alpargatas de eco-cuero o gabardina. Talle 35 - 43", 2000,"./img/alpargata1.png")
+clothing.push(cloth1)
+
+
+// Tejidos
+
+let fabrics = []
+
+const fabr1 = new Products(1,"Poncho","poncho adulto de hilo de 1.8 mts", 6000, "./img/poncho.png")
+clothing.push(fabr1)
+const fabr2 = new Products(2,"Ruana","ruana adulto de hilo de 1.8 mts", 5500,"./img/ruana.png")
+clothing.push(fabr2)
+const fabr3 = new Products(6,"Boina","boinas de hilo de adulto o nio marca -El llano-", 2300, "./img/boina.png")
+clothing.push(fabr3)
+
+// Accesorios
+
+let accessories = []
+
+// STOCK
+const stock = clothing.concat(fabrics, accessories);
 
 // function showCatalog(){
 //     alert(`Podrá ver nuestro catálogo en la consola:`)
@@ -129,12 +144,92 @@ const stock = [product1,product2,product3,product4,product5,product6]
 
 // })
 
+// DARK MODE
 
+let btnDarkMode = document.getElementById("btn__dark__mode")
+let btnLightMode = document.getElementById("btn__light__mode")
+
+let dark__mode
+console.log(localStorage.getItem("dark__mode"))
+if(localStorage.getItem("dark__mode")){
+    dark__mode = localStorage.getItem("dark__mode")
+}else{
+    console.log("entra primera vez")
+    localStorage.setItem("dark__mode", "light")
+} 
+
+if(dark__mode == "dark"){
+    document.body.classList.add("dark__mode")
+}
+
+btnDarkMode.addEventListener("click", ()=>{
+    document.body.classList.add("dark__mode")
+    localStorage.setItem("dark__mode", "dark")
+
+})
+btnLightMode.addEventListener("click", ()=>{
+    document.body.classList.remove("dark__mode")
+    localStorage.setItem("dark__mode", "light")
+})
+
+
+// funcionando
+
+// let articleProducts = document.getElementById("products")
+// articleProducts.setAttribute("class", "productsStyle")
+// function seeProducts(){
+
+//     stock.forEach((prod)=>{
+//         let newProduct = document.createElement("div")
+//         newProduct.innerHTML = ` <article class="card">
+//                                         <h2 class="card__title">${prod.product}</h2>
+//                                         <picture class="hola">
+//                                             <img class="card__img" src="${prod.img}" alt="">
+//                                         </picture>
+//                                         <p class="card__description">${prod.description}</p>
+//                                         <h4 class="card__price">$${prod.price}</h4>
+//                                         <button class="card__button">Agregar al carrito</button>
+//                                     </article>`
+//         articleProducts.appendChild(newProduct)
+    
+//     })  
+    
+// }
+
+// function ignoreProducts(){
+//     articleProducts.innerHTML =""
+// }
+
+// let seeProductsBtn = document.getElementById("seeProducts")
+// seeProductsBtn.addEventListener("click", seeProducts)
+
+// let ignoreProductsBtn = document.getElementById("ignoreProducts")
+// ignoreProductsBtn.onclick = ignoreProducts
+
+
+// prueba
 let articleProducts = document.getElementById("products")
 articleProducts.setAttribute("class", "productsStyle")
-function seeProducts(){
 
-    stock.forEach((prod)=>{
+ let hide__text__btn = document.getElementById ("hide__text__btn")
+
+ let hide__text = document.getElementById("hide__text")
+
+ hide__text__btn.addEventListener("click", toggleText)
+
+
+
+ function toggleText(){
+    hide__text.classList.toggle("show")
+
+    if(hide__text.classList.contains("show")){
+        hide__text__btn.innerHTML = "ver menos"
+    }
+    else {
+        hide__text__btn.innerHTML = "ver mas"
+    }
+
+        stock.forEach((prod)=>{
         let newProduct = document.createElement("div")
         newProduct.innerHTML = ` <article class="card">
                                         <h2 class="card__title">${prod.product}</h2>
@@ -150,14 +245,4 @@ function seeProducts(){
     })  
     
 }
-
-function ignoreProducts(){
-    articleProducts.innerHTML =""
-}
-
-let seeProductsBtn = document.getElementById("seeProducts")
-seeProductsBtn.addEventListener("click", seeProducts)
-
-let ignoreProductsBtn = document.getElementById("ignoreProducts")
-ignoreProductsBtn.onclick = ignoreProducts
-
+ 
